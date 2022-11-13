77caf0716cd2a3a440a02aa775852882f3ca7a3e
Dino <brandoncloutier412@gmail.com>
2022-11-12 21:00:33 -0800

    accordian for transparency policy

---
+++ main.js
@@ -0,0 +1,8 @@
+const collapseItems = document.querySelectorAll('.collapse-item');
+const transparencyPolicyToggle = document.querySelector('#transparencypolicyfooter');
+
+transparencyPolicyToggle.addEventListener('click', () => {
+    collapseItems.forEach(element => {
+        element.classList.toggle('collapse');
+    })
+})
